999d772eca4e9d8124539360392d9437f254e13a
yogesh <yogeshchavan3696@gmail.com>
2021-05-31 22:26:17 +0530

    dataviewlist almost complete

--- routes/dataViewList.js
+++ routes/dataViewList.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const viewList =  require('../models/addNewNewsModels')
 const route = express.Router()
 
-route.get('/dataviewlist',(req,res)=>{
-    res.render('dataviewlist')
+route.get('/dataviewlist', async (req,res)=>{
+    const dataView = new viewList()
+
+    try{
+        const vies = await viewList.find({})
+        console.log(vies)
+        return res.render('dataviewlist',{
+            'userData' : vies
+        })
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
 
 module.exports = route
